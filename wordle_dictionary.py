3d5fc6b02692e3eb7c6dbc4750d0092251b9f216
Nicole Contreras <116455247+nikigcon@users.noreply.github.com>
2023-08-21 23:44:46 -0400

    Add files via upload

---
+++ wordle_dictionary.py
@@ -0,0 +1,124 @@
+words = [ 'abet', 'able', 'ably', 'acai', 'aces', 'ache', 'achy', 'acid', 'acne', 'acre', 'acts', 'adds', 'afar', 'aged', 'ages', 'aide', 'aids', 'ails', 'aims', \
+          'airs', 'airy', 'ajar', 'akin', 'alas', 'ales', 'ally', 'alms', 'aloe', 'also', 'alto', 'alum', 'amen', 'amid', 'ammo', 'amok', 'amps', 'anew', 'ants',\
+          'apes', 'apex', 'apps', 'aqua', 'arch', 'arcs', 'area', 'aria', 'arks', 'arms', 'army', 'arts', 'arty', 'ashy', 'asks', 'atom', 'atop', 'aunt', 'aura', \
+          'auto', 'aver', 'avid', 'avow', 'away', 'awed', 'awes', 'awry', 'axed', 'axel', 'axes', 'axil', 'axis', 'axle', 'axon', 'babe', 'baby', 'back', 'bade', \
+          'bags', 'bail', 'bait', 'bake', 'bald', 'bale', 'balk', 'ball', 'balm', 'band', 'bane', 'bank', 'bans', 'barb', 'bard', 'bare', 'barf', 'bark', 'barn', \
+          'bars', 'base', 'bash', 'bask', 'bass', 'bast', 'bath', 'bats', 'baud', 'bawl', 'bays', 'bead', 'beak', 'beam', 'bean', 'bear', 'beat', 'beau', 'beck', \
+          'beds', 'beef', 'been', 'beep', 'beer', 'bees', 'beet', 'begs', 'bell', 'belt', 'bend', 'bent', 'best', 'beta', 'bets', 'bias', 'bibb', 'bibs', 'bide', \
+          'bids', 'bike', 'bile', 'bill', 'bind', 'bios', 'bird', 'bite', 'bits', 'blab', 'blah', 'bled', 'blew', 'blip', 'blob', 'bloc', 'blog', 'blot', 'blow', \
+          'blue', 'blur', 'boar', 'boas', 'boat', 'bobs', 'bode', 'bods', 'body', 'bogy', 'boil', 'bold', 'bole', 'boll', 'bolt', 'bomb', 'bond', 'bone', 'bong', \
+          'bonk', 'bony', 'boob', 'book', 'boom', 'boon', 'boor', 'boos', 'boot', 'born', 'bore', 'boss', 'both', 'bots', 'bout', 'bowl', 'bows', 'boxy', 'boys', \
+          'bozo', 'brad', 'brag', 'bans', 'bras', 'brat', 'bred', 'brew', 'brie', 'brig', 'brim', 'brit', 'bros', 'brow', 'buck','buds', 'buff', 'bugs', 'bulb', \
+          'bulk', 'bull', 'bump', 'bums', 'bunk', 'buns', 'bunt', 'buoy', 'burl', 'burn', 'burp', 'burr', 'bury', 'bush', 'busk', 'buss', 'bust', 'busy', 'butt', \
+          'buys', 'buzz', 'byes', 'byre', 'byte', 'cabs', 'cads', 'cafe', 'cage', 'cake', 'calf', 'call', 'calm', 'came', 'cami', 'camo', 'camp', 'cane', \
+          'cans', 'cape', 'capo', 'caps', 'carb', 'card', 'care', 'carp', 'cars', 'cart', 'case', 'cash', 'cask', 'cast', 'cats', 'caul', 'cava', 'cave', \
+          'cavy', 'caws', 'cede', 'cell', 'cent', 'chai', 'chap', 'char', 'chat', 'chef', 'chew', 'chic', 'chin', 'chip', 'chit', 'chop', 'chow', 'chug', 'chum', \
+          'cite', 'city', 'clad', 'clam','clan', 'clap', 'claw', 'clay', 'clef', 'clip', 'clod', 'clog', 'clop', 'clot', 'club', 'clue', 'coal', 'coat', 'coax', \
+          'cobs', 'coca', 'coco', 'coda', 'code', 'cods', 'cogs', 'coil', 'coin', 'coir', 'coke', 'cola', 'cold', 'cole', 'colt', 'coma', 'comb', 'come', 'comp', \
+          'cone', 'conk', 'cons', 'cook', 'cool', 'coop', 'coos', 'coot', 'cope', 'cops', 'copy', 'cord', 'core', 'cork', 'corm', 'corn', 'cosh', 'cost','cosy', \
+          'cote', 'cots', 'coup', 'cove', 'cowl', 'cows', 'cozy', 'crab', 'crag', 'cram', 'crap', 'craw', 'crib', 'croc', 'crop', 'crow', 'crud','crus', \
+          'crux', 'cube', 'cues', 'cuff', 'cull', 'cult', 'cups', 'curb', 'curd', 'cure', 'curl', 'curs', 'curt', 'cusp','cuss', \
+          'cute', 'cuts', 'cyan', 'cyst', 'czar', 'dabs', 'dace', 'dada', 'dado', 'dads', 'daft', 'dais', 'dale', 'dame', 'damn', 'damp', 'dams', 'dank', 'dare', \
+          'dark', 'darn', 'dart', 'dash', 'cata', 'date', 'daub', 'dawn', 'days', 'daze', 'dead', 'deaf', 'deal', 'dean', 'dear', 'debt', 'deck', 'deed','deem', \
+          'deep', 'deer', 'deft', 'defy', 'deke', 'deli', 'dell', 'delt', 'demo', 'dens', 'dent', 'deny', 'derm', 'desk', 'dews', 'dewy', 'dhal', 'dhow','dial', \
+          'dice', 'died', 'dies', 'diet', 'digs', 'dill', 'dime', 'dims', 'dine', 'ding', 'dips', 'dire', 'dirt', 'disc', 'dish', 'disk', \
+          'diss', 'diva', 'dive', 'dock', 'docs', 'dodo', 'doer', 'does', 'doge', 'dojo', 'dole', 'doll', 'dolt', 'dome', 'done', 'dong', 'doom','door', 'dope', \
+          'dork', 'dorm', 'dory', 'dose', 'dote', 'doth', 'dots', 'dove', 'down', 'doxy', 'doze', 'dozy', 'drab', 'drag','draw', 'dray', 'drew', 'drip', 'drop', \
+          'drug', 'drum', 'dual', 'duck', 'duct', 'dude', 'duds', 'duel', 'dues', 'duet', 'duff', 'dugs','duke', 'dull', 'duly', 'dumb', 'dune', 'dung', 'dunk', \
+         'duos', 'dupe', 'dusk', 'dust', 'duty', 'dyad', 'dyed', 'dyer', 'dyes', 'each', 'earl', 'earn', 'ears', 'ease', 'east', 'easy', 'eats', \
+          'eave', 'ebbs', 'echo', 'ecru', 'eddy', 'edge', 'edgy', 'edit', 'eels', 'eggs', 'eggy', 'egos','elks', 'ells', 'elms', 'else', 'emit', 'emmy', 'emus', \
+          'ends', 'envy', 'eons', 'epic', 'eras','etch', 'euro','even', 'ever', 'eves', 'evil', 'exam', 'exec', 'exit', 'exon', 'expo', 'eyed', 'eyes', 'face',\
+          'fact', 'fade', 'fads', 'fail', 'fair', 'fake', 'fall', 'fame', 'fang', 'fans', 'fare', 'farm', 'fart', 'fast', 'fate', 'fats','faun', 'fave', 'fawn', \
+          'faze', 'fear', 'feat', 'feck', 'feed', 'feel', 'fees', 'feet', 'fell', 'felt', 'fend', 'ferns', 'fest','feta', 'fete', 'feud', 'fibs', 'fief', 'fife', \
+          'figs', 'file', 'fill', 'film', 'filo', 'find', 'fine', 'fink', 'fins', 'fire', 'firm', 'firs',\
+          'fish', 'fist', 'fits', 'five', 'fizz', 'flab', 'flag', 'flan', 'flap', 'flat', 'flaw', 'flax', 'flay', 'flea', 'fled', 'flee', 'flew', \
+          'flex', 'flip', 'flit', 'floc', 'flog', 'flop', 'flow', 'flux', 'foal', 'foam', 'fobs', 'foes', 'fogs', 'fogy',\
+          'foil', 'fold', 'folk', 'fond', 'font', 'food', 'fool', 'foot', 'fops', 'ford', 'fore', 'fork', 'form', 'fort', 'foul', 'four', 'fowl', 'foxy', 'frag',\
+          'frat', 'fray', 'free', 'fret', 'frig', 'frog', 'from', 'fuel', 'fugu', 'full', 'fume', 'fund', 'funk', 'furl', 'furs', 'fury', 'fuse', 'fuss', 'futz',\
+          'fuzz', 'gaff', 'gage', 'gags', 'gain', 'gait', 'gala', 'gale', 'gall', 'gals', 'game', 'gams', 'gang', 'gaol', 'gape', 'gaps', 'garb', 'gash', 'gasp',\
+          'gate', 'gate', 'gave', 'gawk', 'gawp', 'gays', 'gaze', 'gear', 'geek', 'gels', 'gems', 'gene', 'gent', 'germ',\
+          'give', 'gift', 'gigs', 'gild', 'gill', 'gins', 'gird', 'girl', 'girt', 'gist', 'give',\
+          'glad', 'glam', 'glee', 'glen', 'glia', 'glib', 'glob', 'glom', 'glop', 'glow', 'glue', 'glug', 'glum', 'glut', 'gnar', 'gnat', 'gnaw', 'goad',\
+          'goal', 'goat', 'gobs', 'goby', 'gods', 'goer', 'goes', 'gold', 'golf', 'gone', 'gong', 'good', 'goof', 'gook', 'goon', 'goop', 'goos', 'gore', 'gory',\
+          'gosh', 'goth', 'gout', 'gown', 'goys', 'grab', 'grad', 'gram', 'gran', 'gray', 'gree', 'grew', 'grey', 'grid', 'grim', 'grin', 'grip', 'grit', 'grog',\
+          'grow', 'grub', 'grue', 'guar', 'guff', 'gulf', 'gull', 'gulp', 'gums', 'gunk', 'guns', 'guru', 'gush', 'gust', 'guts', 'guys', 'gyms', 'gyre', 'gyro',\
+          'hack', 'haft', 'hags', 'hail', 'hair', 'hajj', 'hake', 'half', 'hall', 'halo', 'halt', 'hams', 'hand', 'hang', 'hank', 'hard', 'hare', 'hark',\
+          'harm', 'harp', 'hart', 'hash', 'hate', 'hasp', 'hath', 'hats', 'haul', 'have', 'hawk', 'haws', 'hays', 'haze', 'hazy', 'head', 'heal', 'heap',\
+          'hear', 'heat', 'heck', 'heed', 'heel', 'heft', 'heir', 'held', 'hell', 'helm', 'helo', 'help', 'heme', 'hemp', 'hems', 'hens', 'herb', 'herd', 'here',\
+          'hero', 'hers', 'hewn', 'hews', 'hick', 'hide', 'high', 'hike', 'hill', 'hilt', 'hind', 'hint', 'hips', 'hire', 'hiss', 'hits', 'hive', 'hiya',\
+          'hoar', 'hoax', 'hobo', 'hogs', 'hold', 'hole', 'holt', 'holy', 'home', 'hone', 'hong', 'honk', 'hood', 'hoof', 'hook',\
+          'hoot', 'hope', 'hops', 'hora', 'gorn', 'hose', 'host', 'hour', 'hove', 'howl', 'hubs', 'huck', 'hued', 'hues', 'huff', 'huge', 'hugs', 'hula',\
+          'hulk', 'hull', 'hump', 'hums', 'hung', 'hunk', 'hunt', 'hurl', 'hurt', 'hush', 'husk', 'huts', 'hymn', 'hype', 'hypo', 'iced', 'ices',\
+          'icon', 'idea', 'idle', 'idly', 'idol', 'imam', 'imps', 'inch', 'info', 'inks', 'inky', 'into', 'ions', 'iota',\
+          'iris', 'irks', 'iron', 'isle', 'itch', 'item', 'jabs', 'jack', 'jade', 'jail', 'jake', 'jamb', 'jams', 'jape', 'jars', 'java', 'jaws', 'jays',\
+          'jazz', 'jean', 'jeep', 'jeer', 'jeez', 'jefe', 'jell', 'jerk', 'jest', 'jinx', 'jive', 'jobs',\
+          'jock', 'joey', 'jogs', 'john', 'join', 'joke', 'jolt', 'josh', 'jots', 'jowl', 'joys', 'judo', 'judy', 'jump', 'junk', 'jury',\
+          'just', 'kale','kart', 'kata', 'kava', 'keel', 'keen', 'keep', 'kegs', 'kelp', 'kept', 'kerb', 'kerf', 'keto', 'keys',\
+          'khan', 'khat', 'kick', 'kids', 'kill', 'kiln', 'kilo', 'kilo', 'kilt', 'kind', 'king', 'kips', 'kiss', 'kite', 'kith', 'kits', 'kiva', 'kiwi', 'knee',\
+          'knew', 'knit', 'knob', 'knot', 'know', 'koan', 'kohl', 'koji', 'kola', 'kook', 'kora', 'koto', 'kudu', 'labs', 'lace', 'lack', 'lacy', 'lads', 'lady',\
+          'lags', 'laid', 'lain', 'lair', 'lake', 'lakh', 'lama', 'lamb', 'lame', 'lamp', 'lams', 'land', 'lane', 'laps', 'lard', 'lark', 'lash', 'lass', 'last',\
+          'late', 'lath', 'lats', 'laud', 'lava', 'lawn', 'laws', 'lays', 'laze', 'lazy', 'lead', 'leaf', 'leak', 'lean', 'leap', 'leas', 'leek', 'leer', 'leet',\
+          'left', 'legs', 'leis', 'lend', 'lens', 'lent', 'less', 'lest', 'lent', 'less', 'lest', 'levy', 'lewd', 'leys', 'liar', 'libs', 'lice', 'lick', 'lids',\
+          'lied', 'lier', 'lies', 'lieu', 'life', 'lift', 'like', 'lilt', 'lily', 'lima', 'limb', 'lime', 'limn', 'limo', 'limp', 'line', 'ling', 'link',\
+          'lint', 'lion', 'lips', 'lira', 'lisp', 'list', 'ite', 'live', 'load', 'loaf', 'loan', 'loam', 'lobe', 'lobs', 'loch', 'lock', 'loco', 'lode', 'loft',\
+          'loge', 'logo', 'logs', 'loin', 'loll', 'lone', 'long', 'look', 'loom', 'loon', 'loop', 'loos', 'loot', 'lope', 'lops', 'lord', 'lore', 'lose', 'loss',\
+          'lost', 'lots', 'loud', 'loat', 'love', 'lows', 'luau', 'lube', 'luck', 'luff', 'luge', 'lugs', 'lull', 'lump', 'lune', 'lung', 'lure', 'lurk',\
+          'lush', 'lust', 'lute', 'luvs', 'lynx', 'lyre', 'lyre', 'lyse', 'mace', 'mach', 'mack', 'macs', 'made', 'mage', 'magi', 'mags', 'maid', 'mail', 'maim', \
+          'main', 'make', 'maki', 'mako', 'male', 'mall', 'malt', 'mama', 'mane', 'mans', 'many', 'maps', 'mare', 'mark', 'marl', 'mars', 'mart', 'masa', 'mash',\
+          'mask', 'mass', 'mast', 'mate', 'math', 'mats', 'maul', 'maws', 'maxi', 'maya', 'mayo', 'maze', 'mazy', 'mead', 'meal', 'mean', 'meat', 'meek', 'meet',\
+          'mega', 'meld', 'melt', 'memo', 'mend', 'menu', 'meow', 'mere', 'mesa', 'mesh', 'mess', 'meta', 'mete', 'mewl', 'mews', 'meze', 'mica', 'mice', 'mics',\
+          'mids', 'mien', 'mike', 'mild', 'mile', 'milk', 'mill', 'mime', 'mina', 'mind', 'mine', 'mini', 'mink', 'mint', 'minx', 'mire', 'miry', 'mise', 'miso',\
+          'miss', 'mist', 'mite', 'mitt', 'moan', 'moat', 'mobs', 'mock', 'mode', 'mods', 'mojo', 'mola', 'mold', 'mole', 'moll', 'molt', 'moms', 'monks', 'mono',\
+          'mood', 'mook', 'moon', 'moor', 'moos', 'moot', 'mope', 'mops', 'more', 'morn', 'mosh', 'moss', 'most', 'mote', 'moth', 'move', 'mows', 'much', 'muck,',\
+          'muds', 'muff', 'mugs', 'mule', 'mull', 'mums', 'murk', 'muse', 'mush', 'musk', 'muss', 'must', 'mute', 'mutt', 'myth', 'naan', \
+          'nabs', 'naff', 'nags', 'nail', 'name', 'nana', 'nans', 'napa', 'nape', 'naps', 'narc', 'nard', 'nary', 'nave', 'navy', 'nays', 'neap', 'near', 'neat',\
+          'neck', 'need', 'neem', 'nene', 'neon', 'nerd', 'ness', 'nest', 'nets', 'news', 'newt', 'next', 'nibs', 'nick', 'nigh', 'nine', 'nips', 'nite', 'nobs',\
+          'nock', 'node', 'nods', 'noel', 'noir', 'nome', 'none', 'nook', 'noon', 'nope', 'nori', 'norm', 'nose', 'nosh', 'nosy', 'note', 'noun', 'nous', 'nova',\
+          'nubs', 'nude', 'nuke', 'null', 'numb', 'nuns', 'nuts', 'oafs', 'oaks', 'oaky', 'oars', 'oath', 'oats', 'oath', 'obey', 'obit', 'oboe', 'odds', 'odes',\
+          'odor', 'ogee', 'ogle', 'ogre', 'oils', 'oily', 'oink', 'oaky', 'okra', 'olds', 'oldy', 'oleo', 'olla', 'omen', 'omit', 'once', 'ones', 'only', 'onto', \
+          'onus', 'onyx', 'oops', 'ooze', 'oozy', 'opal', 'open', 'opts', 'opus', 'oral', 'orbs', 'orca', 'ordo', 'ores', 'orgy', 'oryx', 'orzo', 'otic', 'otto', \
+          'ouch', 'ours', 'oust', 'outs', 'ouzo', 'oval', 'oven', 'over', 'ovum', 'ovum', 'owed', 'owes', 'owls', 'owns', 'pace', 'pack', 'pact', 'pads', 'page',\
+          'paid', 'pail', 'pain', 'pair', 'pale', 'pall', 'palm', 'palp', 'pals', 'pane', 'pang', 'pans', 'pant', 'papa', 'paps', 'para', 'pare', 'park', 'pars',\
+          'part', 'pass', 'past', 'pate', 'path', 'pats', 'pave', 'pawn', 'paws', 'pays', 'peak', 'peal', 'pear', 'peas', 'peck',\
+          'peek', 'peel', 'peep', 'peer', 'pees', 'pegs', 'pelt', 'pens', 'pent', 'perk', 'perm', 'perp', 'pert', 'peso', 'pest', 'pets',\
+          'pews', 'phew', 'phis', 'pica', 'pice', 'pick', 'pics', 'pied', 'pier', 'pies', 'pigs', 'pika', 'pike', 'pile', 'pill', 'pimp', 'pine', 'ping', 'pink',\
+          'pins', 'pint', 'pion', 'pipa', 'pish', 'piss', 'pita', 'pith', 'pits', 'pity', 'pixy', 'plan', 'plat', 'play', 'plea', 'pleb', 'pled', 'plod',\
+          'plop', 'plot', 'plow', 'ploy', 'plug', 'plum', 'plus', 'pock', 'pods', 'poem', 'poet', 'poke', 'poky', 'pole', 'poll', 'polo', 'pols', 'pome', \
+          'pomp', 'pond', 'pone', 'pong', 'pony', 'poof', 'pool', 'poop', 'poor', 'pope', 'pops', 'pore', 'pork', 'port', 'pose', 'posh', 'post', 'pots',\
+          'pouf', 'pour', 'pout', 'pray', 'prep', 'prim', 'prob', 'prod', 'prom', 'prop', 'pros', 'prow',\
+          'pubs', 'puck', 'puff', 'pugs', 'puja', 'puke', 'pull', 'pulp', 'puma', 'pump', 'punk', 'puns', 'punt', 'puny', 'pupa', 'pups', 'pure',\
+           'purl', 'purr', 'push', 'puss', 'puts', 'putt', 'putz', 'pyre', 'pyro', 'aahed', 'aalii', 'aargh', 'abaca', 'abaci', 'aback', 'baaed', 'babas', 'babel', 'babes', 'babka', 'baboo', 'cabal', 'cabby', 'caber', 'cabin', 'cable',\
+          'daces', 'dacha', 'dadas', 'dados', 'eager', 'eagle', 'eagre', 'eared', 'earls', 'early', 'fable', 'faced', 'facer', 'faces', 'facet', \
+          'facial', 'gabby', 'able', 'gadid', 'gaffe', 'gaffs', 'gaged', 'haafs', 'haars', 'habit', 'hacek', 'hacks', 'hadal', 'iambi', 'iambs', 'ibada', 'ichor',\
+          'icier', 'icily', 'jabot', 'jacal', 'jacks', 'jacky', 'jaded', 'jades', 'kabab', 'kabob', 'kacha', 'kadis', 'kafir', 'kagos', 'laari', 'label', 'labor', \
+          'labra', 'laced', 'lacer', 'abased', 'abaser', 'abases', 'abasia', 'abasic', 'abated', 'abater', 'abates',\
+          'abbeys', 'abbots', 'abduct', 'abided', 'abider', 'abides','abject',  'ablaze', 'ablest', 'aboard',\
+           'babble', 'babied', 'babies', 'babkas', 'baboon', 'cabala', 'cabals', 'cabana', 'dabbed', 'dabber', 'dabble',\
+          'eagers', 'eagled', 'eagles', 'eaglet', 'earbud', 'earful', 'earing', 'fabada', 'fabled', 'fabler',\
+          'fables', 'fabric', 'facade', 'facers', 'facete', 'facets', 'gabber', 'gabble', 'gables',\
+           'habits', 'haboob', 'haceks', 'hachoo', 'hacked', 'abandon', 'abasers', 'abashed', 'babassu', 'babbitt', 'babbled', 'babbler', 'babbles', 'babesia', 'babiche',\
+          'baboons', 'babying', 'cabalas', 'cabanas', 'cabanes', 'cabaret', 'cabbage', 'cabbagy', 'dabbers', 'dabbing', 'dabbled', 'dabbler', 'dabbles',\
+          'dabster', 'dackers', 'dacoits', 'dacoity', 'eagerly', 'eaglets', 'eagling', 'eanling', 'earache', 'earbobs', 'earbuds', 'eardrop', 'eardrum',\
+          'earflap', 'earfuls', 'earings', 'fabadas', 'fablers', 'fabliau', 'fabling', 'fabrics', 'fabular', 'gabbers', 'gabbier', 'gabbing', 'gabbled', \
+         'habitat', 'iambics', 'iceberg', 'iceboat', 'icecaps', 'icefall','iceless', 'jabbing',  \
+          'laagers','labeled', 'labeler', 'between', 'through', 'because', 'however', 'without', 'general', 'another', 'present', 'against', 'example', 'himself', 'thought',\
+          'country', 'history', 'several', 'chapter', 'process', 'certain', 'control', 'whether', 'company', 'society', 'nothing', 'service', 'further', 'subject', 'problem',\
+          'century', 'section', 'already', 'various', 'english', 'perhaps', 'members', 'support', 'results', 'usually', 'special', 'similar', 'brought', 'british', 'natural', 'account',\
+          'changes', 'studies', 'greater', 'foreign', 'working', 'surface', 'limited', 'writing', 'council', 'patient', 'outside', 'instead', 'journal', 'reading', 'disease', 'friends',\
+          'species', 'opinion', 'meaning', 'percent', 'capital', 'private', 'science', 'central', 'believe', 'current', 'should', 'people', 'before', 'during', 'little', 'system', 'number',\
+          'states', 'social', 'within', 'public', 'second', 'though', 'always', 'school', 'united', 'family', 'figure', 'rather', 'course', 'nature', 'having', 'change', 'become', 'period', \
+          'common', 'person', 'either', 'church', 'effect', 'around', 'better', 'itself', 'almost', 'action', 'father', 'making', 'result', 'matter', 'became', 'policy', 'enough', 'mother', 'reason',\
+          'reason', 'health', 'theory', 'office', 'single', 'method', 'report', 'future', 'higher', 'amount', 'really', 'indeed', 'growth', 'market', 'energy', 'living', 'return', 'toward', 'strong',\
+          'letters', 'ground', 'modern', 'moment', 'turned', 'across', 'middle', 'longer', 'spirit', 'simple', 'series', 'source', 'likely', 'behind', 'object', 'twenty', 'income', 'beyond','design',\
+          'except', 'latter', 'direct', 'manner', 'appear', 'anyway', 'which', 'their', 'there', 'would', 'other', 'these', 'about', 'first', 'could', 'after', 'those', 'where', 'being', 'under', 'great'\
+          'state', 'world', 'three', 'while', 'found', 'might', 'still', 'right', 'place', 'every', 'power', 'since', 'given', 'never', 'order', 'water', 'small', 'shall', 'large', 'point', 'again', 'often',\
+          'among', 'house', 'women', 'group', 'think', 'human', 'later', 'until', 'whole', 'early', 'means', 'above', 'value', 'study', 'table', 'known', 'court', 'young', 'white', 'light', 'least', 'level',\
+          'child', 'death', 'press', 'cases', 'going', 'party', 'using', 'sense', 'based', 'whose', 'south', 'total', 'class', 'local', 'along', 'money', 'black', 'force', 'north', 'night', 'short', 'field', \
+          'quite', 'thing', 'woman', 'major', 'third', 'shown', 'began', 'cause', 'heart', 'seems', 'trade', 'clean', 'model', 'apply', 'apple', 'award', 'angry', 'album', 'block', 'beach', 'break', 'brief', \
+          'birth', 'that', 'with', 'from', 'have', 'they', 'were', 'been', 'more', 'when', 'will', 'what', 'them', 'than', 'some', 'time', 'into', 'only', 'such', 'also', 'then', 'most', 'said', 'your', 'very', \
+          'made', 'many', 'over', 'like', 'even', 'well', 'must', 'same', 'work', 'life', 'each', 'much', 'both', 'upon', 'user', 'part', 'good', 'make', 'long', 'just', 'here', 'does', 'down', 'case', 'back', \
+          'know', 'high', 'take', 'year', 'thus', 'form', 'come', 'less', 'last', 'left', 'fact', 'hand', 'came', 'give', 'find', 'need', 'once', 'four', 'home', 'data', 'name', 'body', 'city', 'book', 'side',\
+          'side', 'away', 'best', 'line', 'john', 'days', 'seen', 'land', 'head', 'took', 'went', 'mind', 'next', 'view', 'five', 'love', 'area', 'true', 'self', 'want', 'ever', 'free', 'done', 'king', 'type'
+]
+
+    
